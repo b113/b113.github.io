346e8141a4f73adae34d06c9fe6b192a12d18eae
Sergey Bakhar <b113.bahar@gmail.com>
2017-05-09 21:10:54 +0300

    Masonry final

---
+++ masonry/scripts/script.js
@@ -0,0 +1,42 @@
+$('.grid').masonry({
+	// options
+	itemSelector: '.grid-item',
+	//  columnWidth: 200
+	gutter: 30
+});
+
+
+$('.slider-big').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-small'
+});
+
+
+
+
+$('.slider-small').on('init', function () {
+
+	$('.slick-center').children().css("display", "block");
+
+});
+
+$('.slider-small').on('afterChange', function () {
+	$('.slick-center').siblings().children().css("display", "none");
+	$('.slick-center').children().css("display", "block");
+
+});
+
+$('.slider-small').slick({
+	slidesToShow: 7,
+	slidesToScroll: 1,
+	arrows: false,
+	centerMode: true,
+	focusOnSelect: true,
+	asNavFor: '.slider-big',
+	infinite: true
+
+
+});
