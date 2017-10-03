7175b52dcc61ba511b31f12396d4bcd46228b986
Sergey Bakhar <b113.bahar@gmail.com>
2017-10-04 01:30:38 +0300

    Final

---
+++ prechu/scripts/script.js
@@ -0,0 +1,7 @@
+      	
+$(".naver.animated").naver({
+animated: true, // CSS transition
+label: true , // Display text with mobile handle
+labelOpen: "Close", // Default 'open' label
+	maxWidth: "480px"
+});
